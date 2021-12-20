f3367f5078ad2876ab897ed5e440db5d54c9c90c
andre170594 <andre_pessoa99@hotmail.com>
2021-12-20 20:57:50 +0000

    ficha4 erros resolvidos

--- src/ficha4iteradores/FIcha4.java
+++ src/ficha4iteradores/FIcha4.java
@@ -5,6 +5,38 @@ import java.util.Iterator;
 import java.util.List;
 
 public class FIcha4 {
+
+    // ex7
+    public static Double devolveMaior(Iterable<Double> dr){
+        Iterator<Double> it = dr.iterator();
+        Double maior,tmp;
+        maior = it.next();
+
+        while (it.hasNext()){
+            tmp = it.next();
+            if(tmp > maior)
+                maior = tmp;
+        }
+        return maior;
+    }
+
+    // ex8
+    public static <T extends Comparable<? super T>> T devolveMaiorGEN(Iterable<T> dr){
+        Iterator<T> it = dr.iterator();
+        T maior,tmp;
+        maior =  it.next();
+
+        while (it.hasNext()){
+            tmp =  it.next();
+            if(tmp.compareTo(maior) > 0)
+                maior = tmp;
+        }
+        return maior;
+    }
+
+
+
+
     public static void main(String[] args) {
         System.out.println("ficha4");
 
@@ -41,18 +73,17 @@ public class FIcha4 {
         tabs.add(1.2);
         tabs.add(7.2);
         System.out.println("Maior:" + devolveMaior(tabs));
+
+
+        ArrayList<Integer> tabsInt = new ArrayList<>();
+        tabsInt.add(1);
+        tabsInt.add(2);
+        tabsInt.add(3);
+        tabsInt.add(4);
+        tabsInt.add(5);
+
+        System.out.println("MaiorGEN:" + devolveMaiorGEN(tabsInt));
     }
 
-    public static Double devolveMaior(Iterable<Double> dr){
-        Iterator<Double> it = dr.iterator();
-        Double maior,tmp;
-        maior = it.next();
 
-        while (it.hasNext()){
-            tmp = it.next();
-            if(tmp > maior)
-                maior = tmp;
-        }
-        return maior;
-    }
 }
